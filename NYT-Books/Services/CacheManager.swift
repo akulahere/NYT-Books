1fbb184a0e34132489a7b8063edd8c139e91574b
Dmytro Akulinin <akulinind@gmail.com>
2023-07-14 12:22:22 +0300

    update cache service

--- NYT-Books/Services/CacheManager.swift
+++ NYT-Books/Services/CacheManager.swift
@@ -14,55 +14,57 @@ actor CacheManager {
     private let realm = try! Realm()
     
     func saveCategories(_ categories: [Category]) {
-            let realm = try! Realm()
-            do {
-                try realm.write {
-                    let realmCategories = categories.map { RealmCategory(category: $0) }
-                    realm.add(realmCategories)
-                }
-            } catch {
-                print("Error saving categories: \(error)")
+        let realm = try! Realm()
+        do {
+            try realm.write {
+                let existingCategories = realm.objects(RealmCategory.self)
+                realm.delete(existingCategories)
+                
+                let realmCategories = categories.map { RealmCategory(category: $0) }
+                realm.add(realmCategories)
             }
+        } catch {
+            print("Error saving categories: \(error)")
+        }
     }
     
     // Retrieving categories from the cache
     func getCategories() -> [Category] {
         var categories: [Category] = []
-
-                let realm = try! Realm()
-                let realmCategories = realm.objects(RealmCategory.self)
-                categories = realmCategories.map { Category(realmCategory: $0) }
+        let realm = try! Realm()
+        let realmCategories = realm.objects(RealmCategory.self)
+        categories = realmCategories.map { Category(realmCategory: $0) }
         
         return categories
     }
     
     // Saving books to the cache
     func saveBooks(_ books: [Book], forCategory categoryName: String) {
-            do {
-                let realm = try Realm()
-                try realm.write {
-                    let realmCategory = realm.objects(RealmCategory.self).filter("listNameEncoded == %@", categoryName).first
-                    let existingBookTitles = Set(realmCategory?.books.map { $0.title } ?? [])
-                    
-                    let newBooks = books.filter { !existingBookTitles.contains($0.title) }
-                    let realmBooks = newBooks.map { book -> RealmBook in
-                        let realmBook = RealmBook(book: book)
-                        realmBook.category = realmCategory
-                        realmCategory?.books.append(realmBook)
-                        return realmBook
-                    }
-                    realm.add(realmBooks)
+        do {
+            let realm = try Realm()
+            try realm.write {
+                let realmCategory = realm.objects(RealmCategory.self).filter("listNameEncoded == %@", categoryName).first
+                let existingBookTitles = Set(realmCategory?.books.map { $0.title } ?? [])
+                
+                let newBooks = books.filter { !existingBookTitles.contains($0.title) }
+                let realmBooks = newBooks.map { book -> RealmBook in
+                    let realmBook = RealmBook(book: book)
+                    realmBook.category = realmCategory
+                    realmCategory?.books.append(realmBook)
+                    return realmBook
                 }
-            } catch {
-                print("Error saving books: \(error)")
+                realm.add(realmBooks)
             }
+        } catch {
+            print("Error saving books: \(error)")
+        }
     }
     
     func getBooks(forCategory categoryName: String) -> [Book] {
-            let realm = try! Realm()
-            let realmCategory = realm.objects(RealmCategory.self).filter("listNameEncoded == %@", categoryName).first
-            let realmBooks = realmCategory?.books ?? List<RealmBook>()
-            return realmBooks.map { Book(realmBook: $0) }
-        }
-
+        let realm = try! Realm()
+        let realmCategory = realm.objects(RealmCategory.self).filter("listNameEncoded == %@", categoryName).first
+        let realmBooks = realmCategory?.books ?? List<RealmBook>()
+        return realmBooks.map { Book(realmBook: $0) }
+    }
+    
 }
--- NYT-Books/Services/NetworkService.swift
+++ NYT-Books/Services/NetworkService.swift
@@ -33,6 +33,7 @@ class NetworkService: NetworkServiceProtocol {
         let (data, response) = try await URLSession.shared.data(from: url)
         
         guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            print("Error")
             throw NetworkServiceError.serverError
         }
         do {
